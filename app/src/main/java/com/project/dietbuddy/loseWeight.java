719205c8535679e37082e6efffa6e681ff1b0ba6
youhs1125 <youhs1125@naver.com>
2022-05-15 18:38:40 +0900

    UserInfoJava_v1

--- app/src/main/java/com/project/dietbuddy/loseWeight.java
+++ app/src/main/java/com/project/dietbuddy/loseWeight.java
@@ -1,64 +1,67 @@
 package com.project.dietbuddy;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link loseWeight#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class loseWeight extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class loseWeight extends Fragment{
+    public int week;
+    public int totalCal;
+    public int carb;
+    public int protein;
+    public int fat;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public double goal;
+    public SharedPreferences preferences;
+    public SharedPreferences.Editor editor;
 
-    public loseWeight() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment loseWeight.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static loseWeight newInstance(String param1, String param2) {
-        loseWeight fragment = new loseWeight();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_lose_weight, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lose_weight, container, false);
+        EditText inputWeek = view.findViewById(R.id.inputWeek);
+        EditText inputGoal = view.findViewById(R.id.inputGoal);
+        EditText inputCarb = view.findViewById(R.id.inputCarbo);
+        EditText inputProtein = view.findViewById(R.id.inputProtein);
+        EditText inputFat = view.findViewById(R.id.inputFat);
+
+        TextView calView = view.findViewById(R.id.calResult);
+
+
+
+        preferences = getActivity().getSharedPreferences("PREFS",0);
+        editor = preferences.edit();
+
+        Button calBut = view.findViewById(R.id.calButton);
+
+        calBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editor.putBoolean("cal",true);
+                editor.putString("week",inputWeek.getText().toString());
+                editor.putString("goal",inputGoal.getText().toString());
+
+                editor.commit();
+            }
+        });
+
+        totalCal = preferences.getInt("totalCal",0);
+        carb = preferences.getInt("carb",0);
+        protein = preferences.getInt("protein",0);
+        fat = preferences.getInt("fat",0);
+
+        calView.setText(""+totalCal);
+
+        return view;
     }
 }
