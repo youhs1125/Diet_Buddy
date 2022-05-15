c73792d5815671e089a9fca906c15c027b613a78
pip-izony <eeodqql09@gmail.com>
2022-05-15 19:21:56 +0900

    Calendar Update

--- app/src/main/java/com/project/dietbuddy/fragmentgraph.java
+++ app/src/main/java/com/project/dietbuddy/fragmentgraph.java
@@ -1,5 +1,6 @@
 package com.project.dietbuddy;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -9,8 +10,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +23,7 @@ import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
@@ -31,6 +35,7 @@ public class fragmentgraph extends Fragment {
 	EditText search;
 	Button ok;
 	ListView listView;
+	TextView calendar_tv;
 
 	ArrayList<String> list;
 	ArrayList<String> list_cal;
@@ -47,6 +52,15 @@ public class fragmentgraph extends Fragment {
 	int pro;
 	int fat;
 
+	int y;
+	int m;
+	int d;
+
+	BarChart barChart;
+	ArrayList<BarEntry> entries;
+	BarData data;
+	BarDataSet bardataset;
+
 	private int day;
 	private int day_reset;
 
@@ -61,20 +75,40 @@ public class fragmentgraph extends Fragment {
 		search = view.findViewById(R.id.search);
 		ok = view.findViewById(R.id.ok);
 		listView = view.findViewById(R.id.list);
+		calendar_tv = view.findViewById(R.id.calendar);
+
 		preferences = this.getActivity().getSharedPreferences("PREFS", 0);
 		editor = preferences.edit();
 
 		Calendar calendar = Calendar.getInstance();
 		day = calendar.get(Calendar.DAY_OF_WEEK);
 
+		y = calendar.get(Calendar.YEAR);
+		m = calendar.get(Calendar.MONTH) + 1;
+		d = calendar.get(Calendar.DATE);
+		calendar_tv.setText(y+"-"+m+"-"+d);
+		calendar_tv.invalidate();
+		if(preferences.getString("calendar_date", "") == ""){
+			editor.putString("calendar_date", y+"-"+m+"-"+d);
+			editor.commit();
+		}
+
 		day_reset = preferences.getInt("day", 0);
 		if (day != day_reset) {
-			editor.putString("food", "0");
-			editor.putString("foodcal", "0");
-			editor.putString("foodcarbo", "0");
-			editor.putString("foodpro", "0");
-			editor.putString("foodfat", "0");
-			editor.putString("foodsalt", "0");
+			LinkedHashSet<String> set = new LinkedHashSet<String>();
+			editor.putStringSet("food"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), set);
+			editor.putStringSet("foodcal"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), set);
+			editor.putStringSet("foodcarbo"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), set);
+			editor.putStringSet("foodpro"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), set);
+			editor.putStringSet("foodfat"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), set);
+			editor.putStringSet("foodsalt"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), set);
+
+			editor.putString("foodint"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), "0");
+			editor.putString("foodcalint"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), "0");
+			editor.putString("foodcarboint"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), "0");
+			editor.putString("foodproint"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), "0");
+			editor.putString("foodfatint"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), "0");
+			editor.putString("foodsaltint"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), "0");
 			editor.putInt("day", day);
 			editor.commit();
 		}
@@ -88,6 +122,79 @@ public class fragmentgraph extends Fragment {
 			}
 		});
 
+		calendar_tv.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				showDate();
+			}
+		});
+
+		makeList();
+
+		barChart = (BarChart) view.findViewById(R.id.barchart);
+
+		entries = new ArrayList<>();
+		cal = Integer.parseInt(preferences.getString("foodcalint"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), "0"));
+		carbo = Integer.parseInt(preferences.getString("foodcarboint"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), "0"));
+		pro = Integer.parseInt(preferences.getString("foodproint"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), "0"));
+		fat = Integer.parseInt(preferences.getString("foodfatint"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), "0"));
+		entries.add(new BarEntry(cal, 0));
+		entries.add(new BarEntry(carbo, 1));
+		entries.add(new BarEntry(pro, 2));
+		entries.add(new BarEntry(fat, 3));
+
+		bardataset = new BarDataSet(entries, "Cells");
+
+		ArrayList<String> labels = new ArrayList<String>();
+		labels.add("칼로리");
+		labels.add("탄수화물");
+		labels.add("단백질");
+		labels.add("지방");
+
+		data = new BarData(labels, bardataset);
+		barChart.setData(data); // set the data and list of labels into chart
+		bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+		barChart.animateY(5000);
+		barChart.setDescription("");
+		barChart.getLegend().setEnabled(false);
+
+		return view;
+	}
+	void showDate() {
+		DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+			@Override
+			public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+				y = year;
+				m = month + 1;
+				d = dayOfMonth;
+				calendar_tv.setText(y+"-"+m+"-"+d);
+				calendar_tv.invalidate();
+
+				makeList();
+
+				cal = Integer.parseInt(preferences.getString("foodcalint"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), "0"));
+				carbo = Integer.parseInt(preferences.getString("foodcarboint"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), "0"));
+				pro = Integer.parseInt(preferences.getString("foodproint"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), "0"));
+				fat = Integer.parseInt(preferences.getString("foodfatint"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), "0"));
+				bardataset.removeEntry(0);
+				bardataset.removeEntry(1);
+				bardataset.removeEntry(2);
+				bardataset.removeEntry(3);
+				entries.add(new BarEntry(cal, 0));
+				entries.add(new BarEntry(carbo, 1));
+				entries.add(new BarEntry(pro, 2));
+				entries.add(new BarEntry(fat, 3));
+				barChart.invalidate();
+
+				editor.putString("calendar_date", y+"-"+m+"-"+d);
+				editor.commit();
+			}
+		},y, m - 1, d);
+
+		//datePickerDialog.setMessage("메시지");
+		datePickerDialog.show();
+	}
+	void makeList(){
 		list = new ArrayList<>();
 		list_cal = new ArrayList<>();
 		list_carbo = new ArrayList<>();
@@ -98,14 +205,14 @@ public class fragmentgraph extends Fragment {
 		items = new ArrayList<>();
 		adapter = new NutrientItemAdapter(getActivity(), R.layout.activity_nutrient_item_adapter, items);
 
-		if (preferences.getStringSet("foodgram", null) != null) {
-			list.addAll(preferences.getStringSet("food", null));
-			list_cal.addAll(preferences.getStringSet("foodcal", null));
-			list_carbo.addAll(preferences.getStringSet("foodcarbo", null));
-			list_pro.addAll(preferences.getStringSet("foodpro", null));
-			list_fat.addAll(preferences.getStringSet("foodfat", null));
-			list_salt.addAll(preferences.getStringSet("foodsalt", null));
-			list_gram.addAll(preferences.getStringSet("foodgram", null));
+		if (preferences.getStringSet("foodgram"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), null) != null) {
+			list.addAll(preferences.getStringSet("food"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), null));
+			list_cal.addAll(preferences.getStringSet("foodcal"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), null));
+			list_carbo.addAll(preferences.getStringSet("foodcarbo"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), null));
+			list_pro.addAll(preferences.getStringSet("foodpro"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), null));
+			list_fat.addAll(preferences.getStringSet("foodfat"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), null));
+			list_salt.addAll(preferences.getStringSet("foodsalt"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), null));
+			list_gram.addAll(preferences.getStringSet("foodgram"+Integer.valueOf(y)+Integer.valueOf(m)+Integer.valueOf(d), null));
 			int counter = 0;
 			for(String i : list){
 				String[] title = i.split(",");
@@ -128,34 +235,5 @@ public class fragmentgraph extends Fragment {
 			listView.setBackgroundColor(Color.argb(0, 235,239,242));
 			listView.invalidateViews();
 		}
-
-		BarChart barChart = (BarChart) view.findViewById(R.id.barchart);
-
-		ArrayList<BarEntry> entries = new ArrayList<>();
-		cal = Integer.parseInt(preferences.getString("foodcalint", "0"));
-		carbo = Integer.parseInt(preferences.getString("foodcarboint", "0"));
-		pro = Integer.parseInt(preferences.getString("foodproint", "0"));
-		fat = Integer.parseInt(preferences.getString("foodfatint", "0"));
-		entries.add(new BarEntry(cal, 0));
-		entries.add(new BarEntry(carbo, 1));
-		entries.add(new BarEntry(pro, 2));
-		entries.add(new BarEntry(fat, 3));
-
-		BarDataSet bardataset = new BarDataSet(entries, "Cells");
-
-		ArrayList<String> labels = new ArrayList<String>();
-		labels.add("칼로리");
-		labels.add("탄수화물");
-		labels.add("단백질");
-		labels.add("지방");
-
-		BarData data = new BarData(labels, bardataset);
-		barChart.setData(data); // set the data and list of labels into chart
-		bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
-		barChart.animateY(5000);
-		barChart.setDescription("");
-		barChart.getLegend().setEnabled(false);
-
-		return view;
 	}
 }
